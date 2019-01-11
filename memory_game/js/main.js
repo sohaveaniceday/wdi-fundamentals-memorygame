d8f47a5363fd56a3c07eaf31af0f593ac630bfcb
Richard Turner <rclt201@gmail.com>
2019-01-11 10:42:49 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,16 @@
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
-
-console.log("User flipped " + cardTwo)
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0])
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1])
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert('You found a match!');
+  }
+  else {
+    alert('Sorry, try again.');
+  }
+}
